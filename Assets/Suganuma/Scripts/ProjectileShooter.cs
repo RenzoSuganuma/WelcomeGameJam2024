9fc1f8cfd2f768be4d111293156bb441b030106a
MSI_LapTop_RenzoSuganuma <103351677+RenzoSuganuma@users.noreply.github.com>
2024-05-21 11:52:10 +0900

    [update] 扇形ベクトル：時間経過によって左右に振れる機能の仮実装

---
+++ Assets/Suganuma/Scripts/ProjectileShooter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShooter : MonoBehaviour
+{
+    [SerializeField] private Transform origin;
+    [SerializeField] private float radius;
+
+    private float _elapsedTime = 0;
+    Vector3 _direction = Vector3.zero;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        FindDirection(Mathf.Sin(_elapsedTime) + 90f * Mathf.Deg2Rad, out _direction);
+    }
+
+    // î≠éÀï˚å¸ÇåüçıÇ∑ÇÈ
+    public void FindDirection(float theta, out Vector3 direction)
+    {
+        var h = Mathf.Sin(theta) * radius;
+        var w = Mathf.Cos(theta) * radius;
+
+        direction = new Vector3(h, w, 0) - origin.position;
+
+        Debug.DrawLine(Vector3.zero, new Vector3(w, h, 0));
+    }
+}
