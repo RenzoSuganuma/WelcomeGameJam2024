e4b9b55848d813b31a6059739cba6c29d9379516
aki_ra <a0323w@gmail.com>
2024-05-21 11:35:06 +0900

    [add] AudioSettings

---
+++ Assets/Watanabe/Scripts/Audio/AudioHolder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary> ゲーム内で使用する音をInspectorで設定するためのクラス </summary>
+public class AudioHolder : MonoBehaviour
+{
+    #region Audio Controllers
+    [Serializable]
+    public class BGMController
+    {
+        [SerializeField]
+        private BGMType _bgmType = default;
+        [SerializeField]
+        private AudioClip _bgmClip = default;
+
+        public BGMType BGMType => _bgmType;
+        public AudioClip BGMClip => _bgmClip;
+    }
+
+    [Serializable]
+    public class SEController
+    {
+        [SerializeField]
+        private SEType _seType = default;
+        [SerializeField]
+        private AudioClip _seClip = default;
+
+        public SEType SEType => _seType;
+        public AudioClip SEClip => _seClip;
+    }
+    #endregion
+
+    [SerializeField]
+    private BGMController[] _bgmClips = default;
+    [SerializeField]
+    private SEController[] _seClips = default;
+
+    public BGMController[] BGMClips => _bgmClips;
+    public SEController[] SEClips => _seClips;
+}
+
+public enum BGMType
+{
+    None,
+}
+
+public enum SEType
+{
+    None,
+}
