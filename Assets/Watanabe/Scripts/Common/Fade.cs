ae79fad0783f46f3b50e5008a6036781bb3a63a8
aki_ra <a0323w@gmail.com>
2024-05-21 12:29:32 +0900

    [add] Fade, LoadScene

---
+++ Assets/Watanabe/Scripts/Common/Fade.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary> フェードを管理するクラス </summary>
+public class Fade : MonoBehaviour
+{
+    [Tooltip("フェードさせるUI")]
+    [SerializeField]
+    private Image _fadePanel = default;
+    [Tooltip("実行時間")]
+    [SerializeField]
+    private float _fadeTime = 1f;
+
+    public static Fade Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary> フェードイン開始 </summary>
+    public void StartFadeIn(params Action[] onCompleteFadeIn) => StartCoroutine(FadeIn(onCompleteFadeIn));
+
+    /// <summary> フェードアウト開始 </summary>
+    public void StartFadeOut(params Action[] onCompleteFadeOut) => StartCoroutine(FadeOut(onCompleteFadeOut));
+
+    private IEnumerator FadeIn(params Action[] onCompleteFadeIn)
+    {
+        _fadePanel.gameObject.SetActive(true);
+
+        //α値(透明度)を 1 -> 0 にする(少しずつ明るくする)
+        float alpha = 1f;
+        Color color = _fadePanel.color;
+
+        while (alpha > 0f)
+        {
+            alpha -= Time.deltaTime / _fadeTime;
+
+            if (alpha <= 0f) { alpha = 0f; }
+
+            color.a = alpha;
+            _fadePanel.color = color;
+            yield return null;
+        }
+
+        _fadePanel.gameObject.SetActive(false);
+
+        if (onCompleteFadeIn != null)
+        {
+            foreach (var action in onCompleteFadeIn) { action?.Invoke(); }
+        }
+    }
+
+    private IEnumerator FadeOut(params Action[] onCompleteFadeOut)
+    {
+        _fadePanel.gameObject.SetActive(true);
+
+        //α値(透明度)を 0 -> 1 にする(少しずつ暗くする)
+        float alpha = 0f;
+        Color color = _fadePanel.color;
+
+        while (alpha < 1f)
+        {
+            alpha += Time.deltaTime / _fadeTime;
+
+            if (alpha >= 1f) { alpha = 1f; }
+
+            color.a = alpha;
+            _fadePanel.color = color;
+            yield return null;
+        }
+
+        if (onCompleteFadeOut != null)
+        {
+            foreach (var action in onCompleteFadeOut) { action?.Invoke(); }
+        }
+    }
+}
---
+++ Assets/Watanabe/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using Constants;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader
+{
+    /// <summary> フェード -> シーン遷移 </summary>
+    public static void FadeLoad(SceneName scene) => Fade.Instance.StartFadeOut(() => LoadToScene(scene));
+
+    /// <summary> シーン遷移 </summary>
+    public static void LoadToScene(SceneName scene) => SceneManager.LoadScene(Consts.Scenes[scene]);
+
+    //public void Load()
+    //{
+    //    SceneManager.LoadScene("TitleScene");
+    //}
+}
