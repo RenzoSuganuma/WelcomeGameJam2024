309420320304f5c67314c1c7187ba02d0d8e933d
aki_ra <a0323w@gmail.com>
2024-05-21 14:50:15 +0900

    [add] 雨が降る

---
+++ Assets/Watanabe/Scripts/Weather/Raindrop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Raindrop : MonoBehaviour
+{
+    private ObjectPool _objectPool = default;
+
+    public void Initialize(ObjectPool objectPool)
+    {
+        _objectPool = objectPool;
+    }
+
+    private void OnBecameInvisible()
+    {
+        _objectPool.RemoveObject(gameObject);
+    }
+}
